04b00f5f0ee014737cff2e2abcaa27e82cd2ccbc
Sergey Protko <sergey.protko@yandex.ru>
2024-04-24 22:33:31 +0300

    update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 cp config ~/.config/sway/config
 
-sudo apt install rofi
-sudo apt install grim
-sudo apt install slurp
-sudo apt install blueman
-sudo apt install sway-notification-center
-sudo apt install qasmixer
+sudo apt install -y rofi
+sudo apt install -y grim
+sudo apt install -y slurp
+sudo apt install -y blueman
+sudo apt install -y sway-notification-center
+sudo apt install -y qasmixer
+sudo apt install -y brightnessctl
