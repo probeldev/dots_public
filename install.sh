c44f7cad1694bfd449ed746d6742ad2ef5e314d4
Sergey Protko <sergey.protko@yandex.ru>
2024-04-24 22:02:48 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+cp config ~/.config/sway/config
+
+sudo apt install rofi
+sudo apt install grim
+sudo apt install slurp
+sudo apt install blueman
+sudo apt install sway-notification-center
+sudo apt install qasmixer
