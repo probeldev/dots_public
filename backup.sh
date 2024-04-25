46015f16fc32231014fc391c4944120b2b40b8bf
Sergey Protko <sergey.protko@yandex.ru>
2024-04-25 15:13:20 +0300

    mako

--- backup.sh
+++ backup.sh
@@ -1,3 +1,4 @@
 cp ~/.config/sway/config  config
 cp ~/.config/waybar/style.css  waybar/style.css
 cp ~/.config/waybar/config waybar/config
+cp ~/.config/mako/config mako/config
--- install.sh
+++ install.sh
@@ -5,12 +5,15 @@ mkdir ~/.config/waybar
 cp waybar/style.css ~/.config/waybar/style.css
 cp waybar/config ~/.config/waybar/config
 
+mkdir ~/.config/mako
+cp mako/config ~/.config/mako/config
+
 sudo apt install -y rofi
 sudo apt install -y grim
 sudo apt install -y slurp
 sudo apt install -y blueman
-sudo apt install -y sway-notification-center
 sudo apt install -y qasmixer
 sudo apt install -y brightnessctl
 sudo apt install -y waybar
 sudo apt install -y thunar
+sudo apt install -y mako-notifier
