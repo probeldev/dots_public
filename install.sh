da6420efd90d34896f39607a54612d65da322912
Sergey Protko <sergey.protko@yandex.ru>
2024-05-10 10:44:17 +0300

    thunar to nemo

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ sudo apt install -y slurp
 sudo apt install -y blueman
 sudo apt install -y brightnessctl
 sudo apt install -y waybar
-sudo apt install -y thunar
+sudo apt install -y nemo
 sudo apt install -y mako-notifier
 sudo apt install -y swaylock
 sudo apt install -y pulsemixer
