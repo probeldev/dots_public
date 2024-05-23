cf18c12a1f5cacdb030a16f85959ec6e66fb9338
Sergey Protko <sergey.protko@yandex.ru>
2024-05-23 06:44:39 +0300

    add flatpak, change nemo to dolphin

---
+++ flatpak.sh
@@ -0,0 +1,4 @@
+flatpak install flathub org.kde.dolphin
+flatpak install flathub org.kde.krita
+flatpak install flathub org.libreoffice.LibreOffice
+flatpak install flathub org.gnome.gitlab.somas.Apostrophe
