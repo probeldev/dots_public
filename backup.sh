828e432dd23636fbc323ce29e02dd5c2265566a9
Sergey Protko <sergey.protko@yandex.ru>
2024-08-22 19:55:44 +0300

    change menu app

--- backup.sh
+++ backup.sh
@@ -2,7 +2,6 @@ cp ~/.config/hypr/hyprland.conf hyprland.conf
 cp ~/.config/waybar/style.css waybar/style.css
 cp ~/.config/waybar/config waybar/config
 cp ~/.config/mako/config mako/config
-cp ~/.config/libinput-gestures.conf libinput-gestures.conf
 cp ~/.config/rofi/catppuccin.rasi rofi/catppuccin.rasi
 cp ~/.config/rofi/config.rasi rofi/config.rasi
 rm -rf ranger
