7dee911f03773c65e77190a0a2e06bb39825f6f7
Sergey <probelmc@gmail.com>
2024-10-16 23:51:08 +0300

    fix flatpak install

--- flatpak.sh
+++ flatpak.sh
@@ -1,42 +1,43 @@
 sudo apt install -y flatpak
-flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install -y --user flathub org.kde.krita
-flatpak install -y --user flathub org.libreoffice.LibreOffice
-flatpak install -y --user flathub org.gnome.gitlab.somas.Apostrophe
-flatpak install -y --user flathub org.kde.kwrite
-flatpak install -y --user flathub app.drey.Dialect
-flatpak install -y --user flathub io.github.flattool.Warehouse
-flatpak install -y --user flathub io.github.giantpinkrobots.flatsweep
-flatpak install -y --user flathub org.telegram.desktop
-flatpak install -y --user flathub com.skype.Client
-flatpak install -y --user flathub com.viber.Viber
-flatpak install -y --user flathub net.nokyan.Resources
-flatpak install -y --user flathub org.dbgate.DbGate
-flatpak install -y --user flathub md.obsidian.Obsidian
-flatpak install -y --user flathub com.github.tchx84.Flatseal
-flatpak install -y --user flathub org.kde.ktorrent
+sudo flatpak remote-add --if-not-exists --system  flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-#flatpak install -y --user flathub com.visualstudio.code
-#flatpak install -y --user flathub org.freedesktop.Sdk.Extension.golang # for vs code
+sudo flatpak install -y --system  flathub org.kde.krita
+sudo flatpak install -y --system  flathub org.libreoffice.LibreOffice
+sudo flatpak install -y --system  flathub org.gnome.gitlab.somas.Apostrophe
+sudo flatpak install -y --system  flathub org.kde.kwrite
+sudo flatpak install -y --system  flathub app.drey.Dialect
+sudo flatpak install -y --system  flathub io.github.flattool.Warehouse
+sudo flatpak install -y --system  flathub io.github.giantpinkrobots.flatsweep
+sudo flatpak install -y --system  flathub org.telegram.desktop
+sudo flatpak install -y --system  flathub com.skype.Client
+sudo flatpak install -y --system  flathub com.viber.Viber
+sudo flatpak install -y --system  flathub net.nokyan.Resources
+sudo flatpak install -y --system  flathub org.dbgate.DbGate
+sudo flatpak install -y --system  flathub md.obsidian.Obsidian
+sudo flatpak install -y --system  flathub com.github.tchx84.Flatseal
+sudo flatpak install -y --system  flathub org.kde.ktorrent
 
-flatpak install -y --user flathub io.github.amit9838.mousam
-flatpak install -y --user flathub org.gnome.Podcasts
+#sudo flatpak install -y --system  flathub com.visualstudio.code
+#sudo flatpak install -y --system  flathub org.freedesktop.Sdk.Extension.golang # for vs code
 
-flatpak install -y --user flathub org.gnome.Snapshot
+sudo flatpak install -y --system  flathub io.github.amit9838.mousam
+sudo flatpak install -y --system  flathub org.gnome.Podcasts
 
-flatpak install -y --user flathub org.kde.dolphin
+sudo flatpak install -y --system  flathub org.gnome.Snapshot
+
+sudo flatpak install -y --system  flathub org.kde.dolphin
 gio mime x-directory/normal org.kde.dolphin.desktop # Установка файловым менеджером по умолчанию
 gio mime inode/directory org.kde.dolphin.desktop    # Установка файловым менеджером по умолчанию
 
-flatpak install -y --user flathub org.kde.haruna
+sudo flatpak install -y --system  flathub org.kde.haruna
 
-flatpak install -y --user flathub page.kramo.Sly
-flatpak install -y --user flathub org.kde.okular
-flatpak install -y --user flathub org.kde.gwenview
+sudo flatpak install -y --system  flathub page.kramo.Sly
+sudo flatpak install -y --system  flathub org.kde.okular
+sudo flatpak install -y --system  flathub org.kde.gwenview
 
-flatpak install -y --user flathub io.github.zen_browser.zen
+sudo flatpak install -y --system  flathub io.github.zen_browser.zen
 gio mime x-scheme-handler/http io.github.zen_browser.zen.desktop  # Установка браузером по умолчанию
 gio mime x-scheme-handler/https io.github.zen_browser.zen.desktop # Установка браузером по умолчанию
 
-flatpak install -y --user flathub com.github.tenderowl.frog
+sudo flatpak install -y --system  flathub com.github.tenderowl.frog
