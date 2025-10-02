f4d4c09b26ba5968a159851638f5ffe61883113a
Sergey Protko <sergey.protko@yandex.ru>
2025-10-02 15:08:21 +0300

    disable dock

--- install-mac.sh
+++ install-mac.sh
@@ -16,3 +16,8 @@ defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 # hack for aerospace
 defaults write com.apple.spaces spans-displays -bool true && killall SystemUIServer
 
+
+# disable dock 
+defaults write com.apple.dock autohide-delay -float 1000; killall Dock
+# for restore dock
+#defaults delete com.apple.dock autohide-delay; killall Dock
