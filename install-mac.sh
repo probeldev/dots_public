46783f776f9d45cc04047e2d1b1ae6b29fd9de36
Sergey Protko <sergey.protko@yandex.ru>
2025-11-11 22:52:52 +0300

    update install mac

--- install-mac.sh
+++ install-mac.sh
@@ -10,8 +10,6 @@ cp  aerospace/.aerospace.toml ~/.aerospace.toml
 
 cp  skhd/.skhdrc ~/.skhdrc
 
-# отключения анимаций на маке
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
 # hack for aerospace
 defaults write com.apple.spaces spans-displays -bool true && killall SystemUIServer
