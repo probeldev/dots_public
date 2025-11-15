319c2a84cba1a2d40e778cf5afe83c784efb68fe
Sergey Protko <sergey.protko@yandex.ru>
2025-11-15 14:25:52 +0300

    usage lima for docker

--- docker/mysql/rebuild.sh
+++ docker/mysql/rebuild.sh
@@ -1,9 +1,21 @@
-
 docker stop lamp
 docker stop mysql
 
 docker rm lamp
 docker rm mysql
+
+limactl stop docker
+limactl rm docker
+
+limactl start \
+  --name=docker \
+  --cpus=4 \
+  --memory=1 \
+  template://docker
+
+
+export DOCKER_HOST=$(limactl list docker --format 'unix://{{.Dir}}/sock/docker.sock')
+
 
 docker-compose up -d
 docker exec -it lamp /etc/init.d/apache2 restart
